d8e865f3704aa38f8e287ddda8cddf73c69728e2
Nathan Hietala <Nrhietala@gmail.com>
2021-01-04 13:15:16 -0600

    Register/Login functioning individually

--- potluck-planner/src/validation/schema.js
+++ potluck-planner/src/validation/schema.js
@@ -10,9 +10,6 @@ export default yup.object().shape({
       .string()
       .min(6, "Password must be 6 characters long")
       .required('Please enter a password'),
-
-    // Removed temporarily, interferring with login.
-    // passwordCofirm not required for login, but is for registration
-    // passwordConfirm: yup.string()
-    //   .oneOf([yup.ref('password'), null], 'Passwords must match'),
+    passwordConfirm: yup.string()
+      .oneOf([yup.ref('password'), null], 'Passwords must match'),
 });
