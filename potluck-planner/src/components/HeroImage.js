a62878b905194c1878f92c9b518e7c80004ac70c
Matthew Justice <matthewajustice@gmail.com>
2021-01-05 13:02:47 -0500

    success

---
+++ potluck-planner/src/components/HeroImage.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styled from 'styled-components';
+import heroBg from '../images/family.jpg';
+
+const StyledHeroImg = styled.div`
+  background-image: url(${heroBg});
+  height: 60vh;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border: 2px white;
+  border-style: solid hidden;
+  margin: 5vh 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .title {
+    font-size: 3rem;
+    font-family: droid-sans, sans-serif;
+    padding: 0 30px;
+    width: 60%;
+    min-width: 395px;
+    color: white;
+    border: 2px solid white;
+    background: rgba(104, 120, 100, 0.6);
+    text-shadow: 2.5px 2px 0px #5085a5;
+  }
+`;
+
+export default function HeroImage() {
+  return (
+    <StyledHeroImg alt='A group of people sharing a meal'>
+      <div className='title'>
+        <h2>Potluck planning made easy</h2>
+      </div>
+    </StyledHeroImg>
+  );
+}
--- potluck-planner/src/components/Home.js
+++ potluck-planner/src/components/Home.js
@@ -1,33 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import heroBg from '../images/family.jpg';
+import HeroImage from './HeroImage';
 import bullet from '../images/bullet.png';
-
-const StyledHeroImg = styled.div`
-  background-image: url(${heroBg});
-  height: 60vh;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  border: 2px white;
-  border-style: solid hidden;
-  margin: 5vh 0px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  .title {
-    font-size: 3rem;
-    font-family: droid-sans, sans-serif;
-    padding: 0 30px;
-    width: 60%;
-    min-width: 395px;
-    color: white;
-    border: 2px solid white;
-    background: rgba(104, 120, 100, 0.6);
-    text-shadow: 2.5px 2px 0px #5085a5;
-  }
-`;
 
 const StyledAbout = styled.div`
   margin: 0 auto;
@@ -79,21 +53,12 @@ const StyledAbout = styled.div`
     font-size: 1.3rem;
     letter-spacing: 0.1rem;
   }
-
-  /* __Formatting__ */
-  /* Title */
-  /* background-image:url("../images/family.jpg"); */
-  /* Write up some more info about site features below img */
 `;
 
 function Home() {
   return (
     <div>
-      <StyledHeroImg alt='A group of people sharing a meal'>
-        <div className='title'>
-          <h2>Potluck planning made easy</h2>
-        </div>
-      </StyledHeroImg>
+      <HeroImage />
       <StyledAbout>
         <h2>About us</h2>
         <p>
