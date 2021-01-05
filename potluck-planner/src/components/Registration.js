5ae452782657cddede92576ece2250de8e9f1de2
Matthew Justice <matthewajustice@gmail.com>
2021-01-05 15:18:30 -0500

    registration page styling complete

--- potluck-planner/src/components/Registration.js
+++ potluck-planner/src/components/Registration.js
@@ -4,9 +4,22 @@ import styled from 'styled-components';
 const StyledWrapper = styled.div`
   display: flex;
   flex-direction: column;
+  margin: auto;
+  background-color: #687864;
+  min-height: 50vh;
+  width: 40%;
+  color: #f7f9fb;
+  font-family: droid-sans, sans-serif;
 
   h2 {
-    font-size: 3rem;
+    font-size: 4.5rem;
+    font-family: 'Lobster', cursive;
+    text-shadow: 4px 4px 0px #8fc1e3;
+  }
+
+  .errorWrapper {
+    margin: 30px;
+    font-size: 1.3rem;
   }
 `;
 
@@ -14,7 +27,8 @@ const StyledRegistration = styled.form`
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
-  font-size: 1.7rem;
+  font-size: 1.6rem;
+  text-shadow: 1.5px 0.5px 0px #5085a5;
 
   .error {
     color: #c70000;
@@ -23,28 +37,37 @@ const StyledRegistration = styled.form`
   .formInput {
     margin: 5px;
     margin-left: 20px;
+    border-radius: 7px;
+    border: none;
   }
 
-  #email {
-    margin-left: 77px;
+  .formInput:focus {
+    box-shadow: 0 0 0 3px #8fc1e3;
+    outline: white;
   }
 
   #password {
-    margin-left: 26px;
+    margin-left: 28px;
   }
 
-  .registerFormBtn {
+  .btn {
+    padding: 10px 15px;
+    width: 200px;
+    margin-top: 20px;
+    border: none;
     border-radius: 7px;
     font-size: 1.5rem;
-    font-weight: 600;
-    color: black;
-    margin-top: 30px;
-    background: #56ab2f;
-    // todo adjust color based on button disabled/enabled
+    color: #f7f9fb;
+    background: #8fc1e3;
+  }
 
-    :hover {
-      cursor: pointer;
-    }
+  .btn:focus {
+    box-shadow: 0 0 0 2px #8fc1e3;
+  }
+  .btn:disabled,
+  .btn[disabled] {
+    background-color: #cccccc;
+    color: #666666;
   }
 `;
 
@@ -63,7 +86,7 @@ function Registration(props) {
 
   return (
     <StyledWrapper>
-      <h2>Register now:</h2>
+      <h2>Register:</h2>
       <StyledRegistration onSubmit={submitHandler}>
         <label>
           Username:
@@ -102,7 +125,7 @@ function Registration(props) {
             value={values.password}
           />
         </label>
-        <button className='registerFormBtn' type='submit' disabled={disabled}>
+        <button className='btn' type='submit' disabled={disabled}>
           Create Account
         </button>
       </StyledRegistration>
