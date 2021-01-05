76421835b1578a61c4ca6ab1856a09ba81704f7f
Matthew Justice <matthewajustice@gmail.com>
2021-01-05 10:54:27 -0500

    Undid changes to login and registration files to fix merge issues, didn't wanna mess up nathans work

--- potluck-planner/src/components/Login.js
+++ potluck-planner/src/components/Login.js
@@ -1,83 +1,82 @@
-import React from 'react';
+import React from 'react'
 import styled from 'styled-components';
 
 const StyledLogin = styled.form`
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
-  font-size: 2rem;
+display:flex;
+flex-flow:column nowrap;
+align-items:center;
+font-size: 2rem;
 
-  .error {
-    color: #c70000;
-  }
-
-  .formInput {
-    margin: 5px;
-  }
-
-  .loginFormBtn {
-    padding: 5px 15px;
-    margin: 10px;
-    border-radius: 7px;
-    font-size: 1.5rem;
-    font-weight: 600;
-    color: black;
-    background: #56ab2f;
-    // todo adjust color based on button disabled/enabled
-
-    :hover {
-      cursor: pointer;
-    }
-  }
-`;
-
-function Login(props) {
-  const { values, onChange, onSubmit, disabled, errors } = props;
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-    onSubmit();
-  };
-
-  const changeHandler = (event) => {
-    const { name, value } = event.target;
-    onChange(name, value);
-  };
-
-  return (
-    <StyledLogin className='loginForm' onSubmit={submitHandler}>
-      <h2>Log in:</h2>
-      {errors.username && <div className='error'>{errors.username}</div>}
-      <label className='formLabel'>
-        Username:
-        <input
-          type='text'
-          id='username'
-          className='formInput'
-          name='username'
-          placeholder='Enter Username'
-          onChange={changeHandler}
-          value={values.username}
-        />
-      </label>
-      {errors.password && <div className='error'>{errors.password}</div>}
-      <label className='formLabel'>
-        Password:
-        <input
-          type='password'
-          id='password'
-          className='formInput'
-          name='password'
-          placeholder='Enter Password'
-          onChange={changeHandler}
-          value={values.password}
-        />
-      </label>
-      <button className='loginFormBtn' type='submit' disabled={disabled}>
-        Log In
-      </button>
-    </StyledLogin>
-  );
+.error {
+  color:#c70000;
 }
 
-export default Login;
+.formInput {
+  margin: 5px;
+}
+
+.loginFormBtn {
+  padding: 5px 15px;
+  margin: 10px;
+  border-radius: 7px;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: black;
+  background: #56ab2f;
+  // todo adjust color based on button disabled/enabled
+
+  :hover {
+    cursor: pointer;
+  }
+}
+`
+
+function Login(props) {
+const { values, onChange, onSubmit, disabled, errors } = props;
+
+  const submitHandler = event => {
+   event.preventDefault();
+   onSubmit(); 
+  }
+
+  const changeHandler = event => {
+    const { name, value } = event.target;
+    onChange(name, value);
+  }
+
+  return (
+    <StyledLogin className="loginForm" onSubmit={submitHandler}>
+      <h2>Log in!</h2>
+      {errors.username && <div className="error">{errors.username}</div>}
+      <label className="formLabel">Username:
+        <input 
+        type="text"
+        id="username"
+        className="formInput"
+        name='username'
+        placeholder="Enter Username"
+        onChange={changeHandler}
+        value={values.username}
+        />
+      </label>
+      {errors.password && <div className="error">{errors.password}</div>}
+      <label className="formLabel">Password:
+        <input 
+        type="password"
+        id="password"
+        className="formInput"
+        name='password'
+        placeholder="Enter Password"
+        onChange={changeHandler}
+        value={values.password}
+        />
+      </label>
+      <button className="loginFormBtn"
+      type="submit"
+      disabled={disabled}
+      >Log In</button>
+    </StyledLogin>
+  )
+}
+
+export default Login
