01b7d4edd39cfe31fded09f0adcb303864c07147
Matthew Justice <matthewajustice@gmail.com>
2021-01-08 12:15:22 -0500

    eventcard line 62

--- potluck-planner/src/components/EventCard.js
+++ potluck-planner/src/components/EventCard.js
@@ -1,117 +1,122 @@
-import React,{ useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { UserContext } from '../context/UserContext';
 import { axiosDev } from '../utils/axiosDev';
 
 const StyledEventCard = styled.div`
-display:flex;
-flex-flow:column nowrap;
-justify-content:center;
-background: #31708E;
-border-radius: 15px;
-min-width: 250px;
-margin: 10px;
-padding: 10px;
-border: 2px black solid;
-
-h4 {
-  font-size: 2rem;
-  padding: 5px;
-}
-
-p {
-  font-size: 1.5rem;
-  padding: 5px;
-}
-
-.eventDetails {
-  padding: 5px 10px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  background: #31708e;
+  border-radius: 15px;
+  min-width: 250px;
   margin: 10px;
-  border-radius: 7px;
-  font-size: 1.5rem;
-  font-weight: 600;
-  color: black;
-  background: #87ceeb;
+  padding: 10px;
+  border: 2px black solid;
 
-  :hover {
-    cursor: pointer;
+  h4 {
+    font-size: 2rem;
+    padding: 5px;
   }
-}
 
-.foodItem {
-  font-size: 1.5rem;
-}
+  p {
+    font-size: 1.5rem;
+    padding: 5px;
+  }
 
-.hide {
-  display:none;
-}
+  .eventDetails {
+    padding: 5px 10px;
+    margin: 10px;
+    border-radius: 7px;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: black;
+    background: #87ceeb;
 
-.strike {
-  text-decoration:line-through;
-}
-`
+    :hover {
+      cursor: pointer;
+    }
+  }
+
+  .foodItem {
+    font-size: 1.5rem;
+  }
+
+  .hide {
+    display: none;
+  }
+
+  .strike {
+    text-decoration: line-through;
+  }
+`;
 const initialFoodList = [];
 
 function EventCard(props) {
-  const [ foodList, setFoodList ] = useState(initialFoodList);
+  const [foodList, setFoodList] = useState(initialFoodList);
   const { name, date_time, location } = props.event;
 
-
-  const { loggedInUser } = useContext(UserContext)
-  console.log(loggedInUser)
+  const { loggedInUser } = useContext(UserContext);
+  console.log(loggedInUser);
+  //eslint-disable-next-line
   const { user_id, username } = loggedInUser;
 
   // const dummyFoodItems = ["Baked Beans","Uncle Earl's Chili",'Expired Lima Beans'];
 
   const getFoodList = (event) => {
     const { target } = event;
-    target.classList.add('hide')
+    target.classList.add('hide');
 
-    foodList.map(item => {
-      return foodItem(item.name)
-    })
-  }
+    foodList.map((item) => {
+      return foodItem(item.name);
+    });
+  };
 
   // axios foodlist
   useEffect(() => {
-    axiosDev().get('/api/foods')
-    .then(res => {
-      console.log(res.data);
-      setFoodList(res.data);
-    })
-    .catch(err => {
-      console.log(err)
-    }) 
-  },[])
+    axiosDev()
+      .get('/api/foods')
+      .then((res) => {
+        console.log(res.data);
+        setFoodList(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-  const foodItem = (item)=> {
-    const foodContainer = document.querySelector(".foodContainer");
+  const foodItem = (item) => {
+    const foodContainer = document.querySelector('.foodContainer');
     const div = document.createElement('div');
-    div.classList.add('foodItem')
+    div.classList.add('foodItem');
     div.textContent = item;
 
-    div.addEventListener('click', (event) => {
-      event.target.classList.toggle("strike");
-      const span = document.createElement('span');
-      span.classList.toggle('cook');
-      span.textContent = `Brought by ${username}`;
-      div.parentNode.insertBefore(span, div.nextSibling);
-    },{once:true})
+    div.addEventListener(
+      'click',
+      (event) => {
+        event.target.classList.toggle('strike');
+        const span = document.createElement('span');
+        span.classList.toggle('cook');
+        span.textContent = `Brought by ${username}`;
+        div.parentNode.insertBefore(span, div.nextSibling);
+      },
+      { once: true },
+    );
     foodContainer.append(div);
-  }
+  };
 
   return (
-    <StyledEventCard className="eventCard">
-      <h4 className="title">{name}</h4>
-      <p className="location">Location: {location}</p>
-      <p className="subtitle">Date/Time: {date_time}</p>
-      <button className="eventDetails" onClick={getFoodList}>Details</button>
-      <div className="foodContainer">
-        { }
-      </div>
+    <StyledEventCard className='eventCard'>
+      <h4 className='title'>{name}</h4>
+      <p className='location'>Location: {location}</p>
+      <p className='subtitle'>Date/Time: {date_time}</p>
+      <button className='eventDetails' onClick={getFoodList}>
+        Details
+      </button>
+      <div className='foodContainer'>{}</div>
     </StyledEventCard>
-  )
+  );
 }
 
-export default EventCard
+export default EventCard;
